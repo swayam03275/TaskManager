1c7f093b7ccc02a0372af86530c7755eb8ed54ac
swayam03275 <swayamaggarwal03@gmail.com>
2026-05-01 18:48:27 +0530

    fix5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
-});
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: 'all'
+  }
+})
